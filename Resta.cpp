67e31cd28ce5d24516354b0de66e6348392924e5
Valeria Abigail Vásquez Martínez <00023826@uca.edu.sv>
2026-05-20 16:59:06 -0600

    operacion de la resta

--- Resta.cpp
+++ Resta.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int a, b;
+int a, b, resta;
 
 int main()
 {
-    cout<<"Ingresa dos valores"<< endl;
-    cin>>a;
-    cin>>b;
+    cout << "Ingresa dos valores" << endl;
+    cin >> a;
+    cin >> b;
+
+    resta = a - b;
+
+    cout << "su resultado es de: " << resta << endl;
 
     return 0;
 }
